cc6ef0dc3f1c62fe7e3f79d84e62e317b4b48e6c
judy-26 <svmesvu@gmail.com>
2025-01-06 14:31:53 +0900

    Update NoticeMapper.java

--- kkaebi_yj2/src/main/java/goods/yj/mapper/NoticeMapper.java
+++ kkaebi_yj2/src/main/java/goods/yj/mapper/NoticeMapper.java
@@ -8,16 +8,16 @@ import goods.yj.vo.NoticeVO;
 public interface NoticeMapper {
     
     List<NoticeVO> getAllNotice(); // 전체 조회
+
+    NoticeVO getNoticeDetail(int seq_no); // 상세정보 조회
     
     void updateNotice(NoticeVO notice); // 업데이트
     
     void createNotice(NoticeVO notice); // 생성
     
     void deleteNotice(int seq_no); // 삭제
-    
-    NoticeVO getNoticeDetail(int seq_no); // 상세정보 조회
 
-	void updateExpiredNoticesStatus(String expire_Id);
+    void updateExpiredNoticesStatus(String expire_Id);
 
     
 }
